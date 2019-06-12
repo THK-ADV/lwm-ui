3a0ba76da3709fe56db509bde2877edae3046b9f
Alexander Dobrynin <info@alex-dobrynin.com>
2019-06-12 10:20:56 +0200

    added string interpolation in AuthorityService

--- src/app/services/authority.service.ts
+++ src/app/services/authority.service.ts
@@ -1,17 +1,17 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { AuthorityAtom } from '../models/authorityAtom.model';
-import { Observable } from 'rxjs';
+import {Injectable} from '@angular/core'
+import {HttpClient} from '@angular/common/http'
+import {AuthorityAtom} from '../models/authorityAtom.model'
+import {Observable} from 'rxjs'
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class AuthorityService {
 
-  constructor(private http: HttpClient) { }
- 
-  getAuthorities(systemId: string): Observable<AuthorityAtom[]> {
-    return this.http.get<AuthorityAtom[]>('authorities?systemId=' + systemId);
-  }
-  
+    constructor(private http: HttpClient) {
+    }
+
+    getAuthorities(systemId: string): Observable<AuthorityAtom[]> {
+        return this.http.get<AuthorityAtom[]>(`authorities?systemId=${systemId}`)
+    }
 }
