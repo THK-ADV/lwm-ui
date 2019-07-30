c47a9e2312ac6c7de1413f314290be62ae4f3cf3
Uwe Müsse <uwe.muesse@th-koeln.de>
2019-07-30 17:32:43 +0200

    added lwm-theme

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { User } from '../models/user.model';
 import { AbstractCRUDService } from '../abstract-crud/abstract-crud.service';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -14,7 +15,7 @@ export class UserService implements AbstractCRUDService<User, User> {
   private path = 'users'
 
   get(): Observable<User[]> {
-    return this.http.get<User[]>(this.path);
+    return this.http.get<User[]>(this.path)
   }
   delete(id: string): Observable<User> {
     throw new Error("Method not implemented.");
