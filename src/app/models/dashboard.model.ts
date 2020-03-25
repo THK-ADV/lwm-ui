ddea323972c143065ba1771c2b826633fff281a1
Alexander Dobrynin <info@alex-dobrynin.com>
2020-03-25 16:16:58 +0100

    date and time objects of the student dashboard are now parsed correctly

--- src/app/models/dashboard.model.ts
+++ src/app/models/dashboard.model.ts
@@ -4,34 +4,21 @@ import {LabworkApplicationAtom} from './labwork.application.model'
 import {Semester} from './semester.model'
 import {CourseAtom} from './course.model'
 import {ScheduleEntryAtom} from './schedule-entry.model'
+import {ReportCardEntryAtom} from './report-card-entry.model'
 
 export interface Dashboard {
   status: 'student' | 'employee'
   semester: Semester
 }
 
-// case class StudentDashboard(
-//     user: User,
-//     status: LdapUserStatus,
-//     semester: Semester,
-//     labworks: Seq[LabworkLike],
-//     labworkApplications: Seq[LabworkApplicationLike],
-//     groups: Seq[(String, LabworkLike)],
-//     reportCardEntries: Seq[ReportCardEntryLike],
-//     allEvaluations: Seq[ReportCardEvaluationLike],
-//     passedEvaluations: Seq[(String, String, Boolean, Int)]
-// ) extends Dashboard
-
-export interface StudentDashboard {
-  status: string
-  semester: Semester
-  user?: StudentAtom
-  labworks?: LabworkAtom[]
-  labworkApplications?: LabworkApplicationAtom[]
-  groups?: [string, LabworkAtom][]
-  // cardEntries?: string; // Traversable[ReportCardEntryLike],
-  // evaluations?: string; // Traversable[ReportCardEvaluationLike],
-  // evaluationPatterns?: string; // Traversable[ReportCardEvaluationPattern]
+export interface StudentDashboard extends Dashboard {
+  user: StudentAtom
+  labworks: LabworkAtom[]
+  labworkApplications: LabworkApplicationAtom[]
+  groups: [string, LabworkAtom][]
+  reportCardEntries: ReportCardEntryAtom[]
+  // allEvaluations: TODO
+  // passedEvaluations TODO
 }
 
 export interface EmployeeDashboard extends Dashboard {
--- src/app/services/dashboard.service.ts
+++ src/app/services/dashboard.service.ts
@@ -3,11 +3,20 @@ import {EmployeeDashboard, StudentDashboard} from '../models/dashboard.model'
 import {Observable} from 'rxjs'
 import {HttpService} from './http.service'
 import {SemesterJSON} from '../models/semester.model'
-import {Employee} from '../models/user.model'
+import {Employee, StudentAtom} from '../models/user.model'
 import {CourseAtom} from '../models/course.model'
 import {ScheduleEntryAtomJSON} from '../models/schedule-entry.model'
-import {convertManyScheduleEntries, mapSemesterJSON} from '../utils/http-utils'
+import {
+    convertManyLabworks,
+    convertManyReportCardEntries,
+    convertManyScheduleEntries,
+    mapLabworkJSON,
+    mapSemesterJSON
+} from '../utils/http-utils'
 import {map} from 'rxjs/operators'
+import {LabworkAtomJSON} from '../models/labwork.model'
+import {LabworkApplicationAtom} from '../models/labwork.application.model'
+import {ReportCardEntryAtomJSON} from '../models/report-card-entry.model'
 
 interface DashboardJSON {
     status: 'student' | 'employee'
@@ -18,6 +27,14 @@ interface EmployeeDashboardJSON extends DashboardJSON {
     user: Employee
     courses: CourseAtom[]
     scheduleEntries: ScheduleEntryAtomJSON[]
+}
+
+interface StudentDashboardJSON extends DashboardJSON {
+    user: StudentAtom
+    labworks: LabworkAtomJSON[]
+    labworkApplications: LabworkApplicationAtom[]
+    groups: [string, LabworkAtomJSON][]
+    reportCardEntries: ReportCardEntryAtomJSON[]
 }
 
 @Injectable({
@@ -31,15 +48,24 @@ export class DashboardService {
     private readonly path = 'dashboard'
 
     getStudentDashboard = (): Observable<StudentDashboard> =>
-        this.http.get_<StudentDashboard>(this.path)
+        this.http.get_<StudentDashboardJSON>(this.path)
+            .pipe(map(this.studentDashboardFromJSON))
 
     getEmployeeDashboard = (): Observable<EmployeeDashboard> =>
         this.http.get_<EmployeeDashboardJSON>(this.path)
-            .pipe(map(this.fromJSON))
+            .pipe(map(this.employeeDashboardFromJSON))
 
-    private fromJSON = (x: EmployeeDashboardJSON): EmployeeDashboard => ({
+    private employeeDashboardFromJSON = (x: EmployeeDashboardJSON): EmployeeDashboard => ({
         ...x,
         semester: mapSemesterJSON(x.semester),
         scheduleEntries: convertManyScheduleEntries(x.scheduleEntries)
+    })
+
+    private studentDashboardFromJSON = (x: StudentDashboardJSON): StudentDashboard => ({
+        ...x,
+        semester: mapSemesterJSON(x.semester),
+        labworks: convertManyLabworks(x.labworks),
+        reportCardEntries: convertManyReportCardEntries(x.reportCardEntries),
+        groups: x.groups.map(_ => [_[0], mapLabworkJSON(_[1])])
     })
 }
