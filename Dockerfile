45cc803eb8a27072e071f2c4a9e53d7703c52bb9
Alexander Dobrynin <info@alex-dobrynin.com>
2020-04-01 18:30:29 +0200

    nginx and docker configs

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,3 @@
 FROM nginx:stable-alpine
 
-COPY nginx.conf /etc/nginx/conf.d/default.conf
 COPY dist/lwm-ui /usr/share/nginx/html
--- docker_build.sh
+++ docker_build.sh
@@ -16,16 +16,16 @@ if [ $? -eq 0 ]; then
   if [ $? -eq 0 ]; then
     rm ${packed_name} > /dev/null 2>&1 &
     docker save -o ${packed_name} ${img_name}
-    #if [ $? -eq 0 ]; then
-    #  docker run -p 9001:80 -d --name ${img_name} ${img_name}
+    # if [ $? -eq 0 ]; then
+    #   docker run -p 9001:80 -d --name ${img_name} ${img_name}
     #  if [ $? -eq 0 ]; then
     #    echo ${img_name} is running
     #  else
     #    echoerr docker run failed
     #  fi
-    #else
-    #  echoerr docker save failed
-    # fi
+   # else
+   #   echoerr docker run failed
+   #  fi
   else
     echoerr docker build failed
   fi
