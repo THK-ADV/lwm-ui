f0b000a09d9b9756c3079e6674dce16a2443c0ef
Alexander Dobrynin <info@alex-dobrynin.com>
2020-04-02 11:55:29 +0200

    added url prefix environment to enable local development with local backend server

--- src/app/interceptors/route.interceptor.ts
+++ src/app/interceptors/route.interceptor.ts
@@ -1,20 +1,14 @@
 import {Injectable} from '@angular/core'
 import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http'
 import {Observable} from 'rxjs'
+import {environment} from '../../environments/environment'
 
 @Injectable()
 export class RouteInterceptor implements HttpInterceptor {
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        // if (!req.url.startsWith('http')) {
-        //     const url = `${environment.backendUrl}:${environment.backendPort}/`
-        //     req = req.clone({
-        //         url: url + req.url
-        //     })
-        // }
-
         req = req.clone({
-            url: `api/${req.url}`
+            url: `${environment.backend_url_prefix}/${req.url}`
         })
 
         return next.handle(req)
