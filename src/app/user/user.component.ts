0e644728643076333a450b51cca73de61df3fdb2
Alexander Dobrynin <info@alex-dobrynin.com>
2021-02-04 15:45:07 +0100

    users can now be updated (#78)

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,10 +1,16 @@
 import {Component} from '@angular/core'
-import {TableHeaderColumn} from '../abstract-crud/abstract-crud.component'
+import {Creatable, TableHeaderColumn} from '../abstract-crud/abstract-crud.component'
 import {Observable} from 'rxjs'
-import {User} from '../models/user.model'
-import {UserService} from '../services/user.service'
-import {LWMActionType} from '../table-action-button/lwm-actions'
-import {MatTableDataSource} from '@angular/material'
+import {Employee, Lecturer, StudentAtom, User} from '../models/user.model'
+import {EmployeeProtocol, isStudentProtocol, StudentProtocol, UserService} from '../services/user.service'
+import {FormInputString} from '../shared-dialogs/forms/form.input.string'
+import {FormInputOption} from '../shared-dialogs/forms/form.input.option'
+import {invalidChoiceKey} from '../utils/form.validator'
+import {Degree} from '../models/degree.model'
+import {DegreeService} from '../services/degree.service'
+import {isStudentAtom} from '../utils/type.check.utils'
+import {isUniqueEntity} from '../models/unique.entity.model'
+
 
 @Component({
     selector: 'lwm-user',
@@ -14,25 +20,99 @@ import {MatTableDataSource} from '@angular/material'
 export class UserComponent {
 
     columns: TableHeaderColumn[]
-    users$: Observable<User[]>
+    users$: Observable<StudentAtom | Employee | Lecturer[]>
+    creatable: Creatable<StudentProtocol | EmployeeProtocol, StudentAtom | Employee | Lecturer>
 
-    private dataSource: MatTableDataSource<User>
-
-    constructor(private readonly service: UserService) {
+    constructor(
+        private readonly service: UserService,
+        private readonly degreeService: DegreeService,
+    ) {
         this.columns = [
             {attr: 'lastname', title: 'Nachname'},
             {attr: 'firstname', title: 'Vorname'},
             {attr: 'systemId', title: 'GMID'},
-            {attr: 'email', title: 'Email'}
+            {attr: 'email', title: 'Email'},
+            {attr: 'enrollment', title: 'Studiengang'},
         ]
 
-        this.users$ = service.getAll()
+        this.users$ = service.getAllAtomic()
+
+        this.creatable = {
+            dialogTitle: 'Benutzer',
+            emptyProtocol: () => ({
+                email: '',
+                enrollment: '',
+                firstname: '',
+                lastname: '',
+                registrationId: '',
+                systemId: ''
+            }),
+            makeInput: (attr, d) => {
+                if (!isUniqueEntity(d)) {
+                    throw new Error('only updates are supported')
+                }
+
+                switch (attr) {
+                    case 'systemId':
+                        return {isDisabled: true, data: new FormInputString(d.systemId)}
+                    case 'email':
+                        return {isDisabled: true, data: new FormInputString(d.email)}
+                    case 'firstname':
+                        return {isDisabled: true, data: new FormInputString(d.firstname)}
+                    case 'lastname':
+                        return {isDisabled: false, data: new FormInputString(d.lastname)}
+                }
+
+                if (attr === 'enrollment' && isStudentAtom(d)) {
+                    return {
+                        isDisabled: false,
+                        data: new FormInputOption<Degree>(
+                            'enrollment',
+                            invalidChoiceKey,
+                            true,
+                            x => x.label,
+                            degreeService.getAll(),
+                            200,
+                            xs => xs.find(x => x.id === d.enrollment.id)
+                        )
+                    }
+                }
+
+                return undefined
+            },
+            commitProtocol: (p, d) => {
+                if (!d) {
+                    throw new Error('only updates are supported')
+                }
+
+                if (isStudentAtom(d) && isStudentProtocol(p)) {
+                    return {
+                        lastname: p.lastname,
+                        enrollment: p.enrollment,
+                        systemId: d.systemId,
+                        firstname: d.firstname,
+                        email: d.email,
+                        registrationId: d.registrationId
+                    }
+                } else {
+                    return {
+                        lastname: p.lastname,
+                        systemId: d.systemId,
+                        firstname: d.firstname,
+                        email: d.email
+                    }
+                }
+            },
+            update: service.update
+        }
     }
 
-    initDataSource = (ds: MatTableDataSource<User>) =>
-        this.dataSource = ds
-
-    syncAction = (): LWMActionType[] => ['sync']
-
-    onSync = () => console.log('sync...') // TODO
+    tableContent = (model: Readonly<User>, attr: string): string => {
+        switch (attr) {
+            case 'enrollment':
+                return isStudentAtom(model) ? model.enrollment.label : ''
+            default:
+                return model[attr]
+        }
+    }
 }
